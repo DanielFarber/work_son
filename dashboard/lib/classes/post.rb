bf0c43b25d98e9826ae4e2e4337ffc7fae805da6
DanielFarber <farber.daniel.m@gmail.com>
2014-09-09 08:56:04 -0400

    Pages now nest links in anchor tags

--- dashboard/lib/classes/post.rb
+++ dashboard/lib/classes/post.rb
@@ -1,4 +1,15 @@
 require "active_record"
 
 class Post < ActiveRecord::Base
+
+	def self.link_to_text(str)
+		if str.downcase.include?("http")
+			links = str.scan(/http\S+/)
+			links.each do |link|
+				str = str.gsub(link, "<a href='#{link}'>#{link}</a>")
+			end
+		end
+		str
+	end
+
 end
